cd120c8eb355ff1f220905092eb795b3cbb30998
Katherine <krosenk729@gmail.com>
2017-12-15 15:48:47 -0500

    well. this works

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,5 @@
-var ansShowTime = 3000;
-var questTime = 30000;
+var ansShowTime = 3; 
+var questTime = 20; //time per question in seconds
 var allQuestions = loadQs(); // array of all questions to pull from
 var gameLength = 3; // number of questions in a game
 
@@ -13,17 +13,139 @@ $(document).ready(function(){
 		nextA( $(this).data('answer') );
 	});
 
-	$('#pause-play').click(pauseQ);
+	$('#pause-play').click(function(){
+		questTimer.pauseQ();
+		changeCard('.card-pause');
+	});
 
-	$('#unpause').click(timeQ);
+	$('#unpause').click(function(){
+		questTimer.timeQ(false);
+		changeCard('.card-question');
+	});
+
+	/***********************************************/
+	// game variables
+	var pos = 1; // position in game 
+	var ansCor, ansIncor, ansTimed, usersTime; // number of correct answers, incorrect answers, time outs, timing function
+	var gameQuestions; 
+	var questTimer = {
+	  isRunning: false,
+	  time: questTime,
+	  
+	  timeQ: function(isNew) {
+	  	if( isNew ){
+	  		questTimer.time = questTime;
+	  		$('.counting').html( questTimer.time );
+	  	}
+	    if( !questTimer.isRunning ) {
+	        usersTime = setInterval(questTimer.count, 1000);
+	        questTimer.isRunning = true;
+	    }
+	  },
+
+	  pauseQ: function() {
+	    clearInterval(usersTime);
+	    questTimer.isRunning = false;
+
+	  },
+
+	  count: function() {
+	  	questTimer.time = Math.max(0, questTimer.time - 1);
+	  	$('.counting').html( questTimer.time );
+
+	  	if(questTimer.time === 0 ){
+	  		questTimer.pauseQ();
+	  		nextA();
+	  	}
+	  }
+	};
+
+	/***********************************************/
+	// game functions
+
+	// function to reset game and start a new one 
+	function newG(){
+		[pos, ansCor, ansIncor, ansTimed, usersTime] = [1, 0, 0, 0, {}];
+		$('#timedout, #correct, #incorrect').text('0');
+
+		$('#game-over').hide();
+
+		gameQuestions = randArr(allQuestions).slice(0, gameLength);
+
+		nextQ(pos);
+	}
+
+	// function to advance game to question at passed position
+	function nextQ(pos){
+		clearQ(); 
+
+		var q = gameQuestions[ pos-1 ];
+		$('.game-question').html( q.definition );
+
+		var a = [q.name, q.notA, q.notB, q.notC];
+		randArr(a);
+
+		a.forEach(function(item, index){
+			var v = '';
+			v += '<button id="Q' + index + '" data-answer="' + item + '" class="btn seuss-btn">';
+			v += item + '</button>';
+			$('.form-group').append(v);
+		});
+
+		$('.progress-bar').css('width', ( pos / gameLength * 100 ) + '%').html('<h5>' + pos + ' out of ' + gameLength + '</h5>' );
+
+		changeCard('.card-question');
+		questTimer.timeQ( true );
+
+	}
+
+	// function to clear UI of past questions
+	function clearQ(){
+		$('.game-question, .form-group').empty();
+		questTimer.pauseQ();
+	}
+
+	// function to transition between cards
+	function changeCard(toCard){
+		$('.card-welcome, .card-pause, .card-question, .card-score').hide();
+		$(toCard).fadeIn();
+	}
+
+
+	// function to show a question's outcome
+	function nextA(userInput){
+		questTimer.pauseQ();
+		switch(userInput){
+			case undefined:
+				ansTimed ++;
+				$('#timedout').text(ansTimed);
+				$('#score-message').html('How did it get so late so soon? <br>The clock counted down before you answered');
+				break;
+			case gameQuestions[pos-1].name:
+				ansCor ++;
+				$('#correct').text(ansCor);
+				$('#score-message').html('You have brains in your head');
+				break;
+			default:
+				ansIncor ++;
+				$('#incorrect').text(ansIncor);
+				$('#score-message').html('That wasn\'t right... you should have said '+ gameQuestions[pos-1].name + '<br>Oh, the thinks you can think up if only you try!');
+		}
+
+		changeCard('.card-score');
+
+		if( pos < gameLength ){
+			pos ++;
+			setTimeout(function(){ nextQ(pos); }, ansShowTime*1000 );
+		} else {
+			$('#game-over').show();
+		}
+	}
 
 });
 
-var pos = 1; // position in game 
-var ansCor, ansIncor, ansTimed; // number of correct answers, incorrect and timed out
-var gameQuestions; 
-var questTimer;
-
+/***********************************************/
+// generic functions 
 // function to get all questions
 function loadQs(){
 	var t = $.ajax({
@@ -33,111 +155,6 @@ function loadQs(){
 		contentType: 'application/json'
 	});
 	return t.responseJSON.AllQuestions;
-
-	/*
-	var quests;
-	var xhttp = new XMLHttpRequest();
-	xhttp.onreadystatechange = function(){
-		if(this.readyState == 4 && this.status == 200){
-			quests = responseText;
-		}
-	};
-	xhttp.open('GET','assets/javascript/quizquestions.json');
-	xhttp.send();
-	*/
-}
-
-// function to reset game and start a new one 
-function newG(){
-	[pos, ansCor, ansIncor, ansTimed] = [1, 0, 0, 0];
-
-	$('#game-over').hide();
-
-	gameQuestions = randArr(allQuestions).slice(0, gameLength);
-
-	nextQ(pos);
-}
-
-// function to advance game to question at passed position
-function nextQ(pos){
-	clearQ(); 
-	var q = gameQuestions[ pos-1 ];
-	$('.game-question').html( q.definition );
-
-	var a = [q.name, q.notA, q.notB, q.notC];
-	randArr(a);
-
-	a.forEach(function(item, index){
-		var v = '';
-		v += '<button id="Q' + index + '" data-answer="' + item + '" class="btn btn-info">';
-		v += item + '</button>';
-		$('.form-group').append(v);
-	});
-
-	$('.progress-bar').css('width', '\'' + ( pos / gameLength ) + '%\'');
-
-	changeCard('.card-question');
-	timeQ();
-}
-
-// function to time a question
-function timeQ(){
-	var t = {time: questTime};
-	$('.counting').html( t );
-
-	questTimer = setInterval(down, 1000);
-
-	function down(){
-		t -= 1000;
-		$('.counting').html( t );
-	}
-
-}
-
-function pauseQ(){
-
-}
-
-// function to clear UI of past questions
-function clearQ(){
-	$('.game-question, .form-group').empty();
-}
-
-
-// function to transition between cards
-function changeCard(toCard){
-	$('.card-welcome, .card-question, .card-score').hide();
-	$(toCard).fadeIn();
-}
-
-
-// function to show a question's outcome
-function nextA(userInput){
-	switch(userInput){
-		case undefined:
-			ansTimed ++;
-			$('#timedout').text(ansTimed);
-			$('#score-message').html('How did it get so late so soon? <br>The clock counted down before you answered');
-			break;
-		case gameQuestions[pos-1].name:
-			ansCor ++;
-			$('#correct').text(ansCor);
-			$('#score-message').html('You have brains in your head');
-			break;
-		default:
-			ansIncor ++;
-			$('#incorrect').text(ansIncor);
-			$('#score-message').html('That wasn\'t right... you should have said '+ gameQuestions[pos-1].name + '<br>Oh, the thinks you can think up if only you try!');
-	}
-	
-	changeCard('.card-score');
-
-	if( pos === gameLength ){
-		$('#game-over').show();
-	} else {
-		pos ++;
-		setTimeout(function(){ nextQ(pos); }, ansShowTime);
-	}
 }
 
 // function to randomly sort an array
